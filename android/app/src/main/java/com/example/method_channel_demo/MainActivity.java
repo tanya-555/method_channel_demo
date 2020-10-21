59915590114ad551035dd3731de846151a67f8e9
Tanya <tanyacarmel555@gmail.com>
2020-10-21 13:22:33 +0530

    send data from native

---
+++ android/app/src/main/java/com/example/method_channel_demo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.method_channel_demo;
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "samples.flutter.dev/channel";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            result.success("Hello from Native");
+                        }
+                );
+    }
+
+}
